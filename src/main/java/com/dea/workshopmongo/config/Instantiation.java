3c15e208f3f32d6964a5d42aeff073d544db9d3e
eduardodea85 <eduardodea@outlook.com>
2023-05-29 00:05:11 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/dea/workshopmongo/config/Instantiation.java
+++ src/main/java/com/dea/workshopmongo/config/Instantiation.java
@@ -1,30 +1,45 @@
 package com.dea.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.dea.workshopmongo.domain.Post;
 import com.dea.workshopmongo.domain.User;
+import com.dea.workshopmongo.repository.PostRepository;
 import com.dea.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userRepository;//Repository injetado
+	
+	@Autowired
+	private PostRepository postRepository;//Repository injetado
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		userRepository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		userRepository.deleteAll();//Limpa as coleções
+		postRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");//Instancia os usuários
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);//Instancia os post associando com o autor.
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));//Salva tudo.
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
---
+++ src/main/java/com/dea/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,23 @@
+package com.dea.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dea.workshopmongo.domain.Post;
+
+//implementando o repository usando o springdata colocando a anotacao Repository
+
+@Repository // Em UserResource, refatorar o código, usando o UserService para buscar os
+			// //usuários
+public interface PostRepository extends MongoRepository<Post, String> { // Essa interface exetende ou herda da interface
+																		// MongoRepository. Esse MongoRepository precisa
+																		// de dois dados, o tipo da classe de dominio
+																		// que vai gerenciar (Nesse caso é um
+																		// UserRepositoty para gerenciar classes User).
+																		// Em segundo lugar preciso dizer qual o tipo do
+																		// ID dessa classe. (Nesse caso o tipo do id é
+																		// String).
+
+}
+
+//Somente com isso um objeto UserRepository vai ser capaz de fazer varias operações básicas com os meus usuários (salvar, recuperar, deletar, atuaizar), tudo isso já está embutido no MongoRepository.
