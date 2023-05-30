25537b6f06d6985e1c28534da7741085806b4789
eduardodea85 <eduardodea@outlook.com>
2023-05-30 03:13:21 -0300

    Consulta simples com query methods

--- src/main/java/com/dea/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dea/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.dea.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,17 +9,10 @@ import com.dea.workshopmongo.domain.Post;
 
 //implementando o repository usando o springdata colocando a anotacao Repository
 
-@Repository // Em UserResource, refatorar o código, usando o UserService para buscar os
-			// //usuários
-public interface PostRepository extends MongoRepository<Post, String> { // Essa interface exetende ou herda da interface
-																		// MongoRepository. Esse MongoRepository precisa
-																		// de dois dados, o tipo da classe de dominio
-																		// que vai gerenciar (Nesse caso é um
-																		// UserRepositoty para gerenciar classes User).
-																		// Em segundo lugar preciso dizer qual o tipo do
-																		// ID dessa classe. (Nesse caso o tipo do id é
-																		// String).
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
 
-//Somente com isso um objeto UserRepository vai ser capaz de fazer varias operações básicas com os meus usuários (salvar, recuperar, deletar, atuaizar), tudo isso já está embutido no MongoRepository.
